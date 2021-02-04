44bb8c4dcc6e84bac878c27185050953086afeff
TiagoZD <57190443+TiagoZD@users.noreply.github.com>
2021-02-04 18:58:01 -0300

    Add routing (#3)

---
+++ src/pages/HomePage.js
@@ -0,0 +1,10 @@
+import { Home } from 'components/Home';
+import { MainLayout } from 'layouts/MainLayout';
+
+const HomePage = () => (
+  <MainLayout>
+    <Home />
+  </MainLayout>
+);
+
+export default HomePage;
---
+++ src/routes/routes.js
@@ -0,0 +1,10 @@
+import { Home } from 'components/Home';
+
+export const routes = [
+  {
+    children: <Home />,
+    key: 'home',
+    name: 'Home',
+    path: '/',
+  },
+];
